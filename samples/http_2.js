69de0e2e93e481ae736d3e560ed2fabc5b04bc3e
Mihail Stoykov <M.Stoikov@gmail.com>
2021-02-25 14:03:57 +0200

    revert changes to a sample script

--- samples/http_2.js
+++ samples/http_2.js
@@ -1,16 +1,9 @@
 import http from "k6/http";
 import { check } from "k6";
 
-if (__VU == 1) {
-    http.setResponseCallback(http.expectedStatuses(200))
-} else {
-    http.setResponseCallback(http.expectedStatuses(300))
-}
 export default function () {
-    console.log(JSON.stringify(http));
   check(http.get("https://test-api.k6.io/"), {
     "status is 200": (r) => r.status == 200,
     "protocol is HTTP/2": (r) => r.proto == "HTTP/2.0",
   });
-
 }
